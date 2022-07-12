6405d0dd4c8512e99390e88a1d2cbc1163b816f2
Sainath Paruchuri <67890976+sai530534@users.noreply.github.com>
2022-07-12 12:08:15 -0400

    Add files via upload

---
+++ Natu.java
@@ -0,0 +1,14 @@
+package Preparation;
+import java.util.*;
+public class Natu {
+	
+	public static void main(String[] args) {
+		
+		Scanner r =new Scanner(System.in);
+		int a=r.nextInt();
+		int b=(a*(a+1))/2;
+	System.out.println(b);
+	
+	
+	}
+}
