1efa14b16230a045105896999c1aee54650ecc90
Sainath Paruchuri <67890976+sai530534@users.noreply.github.com>
2022-07-12 12:45:28 -0400

    Add files via upload

---
+++ java/buy.java
@@ -0,0 +1,17 @@
+package Preparation;
+import java.util.*;
+public class buy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sr= new Scanner(System.in);
+int a,b,c,d;
+a=sr.nextInt();
+b=sr.nextInt();
+c=sr.nextInt();
+d=sr.nextInt();
+int e= (a*c)+(b*d);
+System.out.println(e);
+	}
+
+}
